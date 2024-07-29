2514fc2f64513388ed5995ba97fb8b38ef556a9a
sarahayman22 <saraaymanabushinnar.com>
2024-07-29 18:16:39 +0300

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    //return an http response to the client 
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿//dotnet class
+
+//logical naming structure
+namespace API.Entities;
+
+//if i want to call this class just inside same namespace (API)
+public class AppUser
+{
+//each of this proprties represent a column
+//defult value of int =0 
+//def value of refrence type (string)
+public int Id { get; set; }
+//? mean optional 
+public required string UserName { get; set; }
+ 
+}
